9e6e4e1c8205ba343dfff38666fa18fb16387a16
unknown <RainbowWolfer@outlook.com>
2022-07-13 12:36:08 +0800

    Analog Fix

--- Analog.xaml.cs
+++ Analog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -27,10 +28,14 @@ namespace DesktopImmersiveClock {
 
 		public void Update() {
 			int hour = DateTime.Now.Hour;//24
+			if(hour >= 12) {
+				hour -= 12;
+			}
 			int min = DateTime.Now.Minute;//60
 			int sec = DateTime.Now.Second;//60
 
-			Hour.Rotation = 360 * hour / 24d;
+			Hour.Rotation = 360 * hour / 12d;
+			Debug.WriteLine($"{hour}-{Hour.Rotation}");
 			Minute.Rotation = 360 * min / 60d;
 			Second.Rotation = 360 * sec / 60d;
 
